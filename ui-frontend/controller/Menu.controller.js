34577e6764daf5117b5792ee246407022f9c7a8c
dmitry <dmitry.zyryanov@execution.su>
2018-05-08 15:44:26 +0700

    navigation: getting router extracted

--- ui-frontend/controller/Menu.controller.js
+++ ui-frontend/controller/Menu.controller.js
@@ -5,8 +5,11 @@ sap.ui.define([
     "use strict";
     return Controller.extend("personal.account.controller.Menu", {
         onInit: function () {
-            this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            this.oRouter.getRoute("menuPage").attachMatched(this._onRouteMatched,this);
+            this.getRouter().getRoute("menuPage").attachMatched(this._onRouteMatched,this);
+        },
+
+        getRouter() {
+            return sap.ui.core.UIComponent.getRouterFor(this);
         },
 
         _onRouteMatched: function (oEvent) {
@@ -20,17 +23,16 @@ sap.ui.define([
                 oComponent.getModel("techModel").setProperty("/tech/selectedKey", tab);
                 oNavCon.to(this.getView().byId(tab),"show");
             } else {
-                Utils.navigateToMenuPageTab(this.oRouter, "Profile");
+                Utils.navigateToMenuPageTab(this.getRouter(), "Profile");
             }
         },
 
         onSelectTab: function (oEvent) {
-            Utils.navigateToMenuPageTab(this.oRouter, oEvent.getParameter("selectedKey"));
+            Utils.navigateToMenuPageTab(this.getRouter(), oEvent.getParameter("selectedKey"));
         },
 
         onHomePress: function () {
-            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            Utils.navigateToMenuPageTab(oRouter, "Profile");
+            Utils.navigateToMenuPageTab(this.getRouter(), "Profile");
         }
     });
 });
