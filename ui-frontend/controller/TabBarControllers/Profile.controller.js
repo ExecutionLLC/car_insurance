fb22e3e18ffe85dc120a2afd1728f46cbde8f7e7
dmitry <dmitry.zyryanov@execution.su>
2018-05-10 17:42:16 +0700

    profile page: unused code removed

--- ui-frontend/controller/TabBarControllers/Profile.controller.js
+++ ui-frontend/controller/TabBarControllers/Profile.controller.js
@@ -6,24 +6,6 @@ sap.ui.define([
 ], function (Controller, formatter, Utils, Const) {
     "use strict";
     return Controller.extend("personal.account.controller.TabBarControllers.Profile", {
-        formatter: formatter,
-
-        onInit: function () {
-            var oComponent = this.getOwnerComponent();
-            this.oTechModel = oComponent.getModel("techModel");
-            this.oMainModel = oComponent.getModel("mainModel");
-            var mainModelBinding = new sap.ui.model.Binding(
-                this.oMainModel, "/", this.oMainModel.getContext("/")
-            );
-            mainModelBinding.attachChange(this.onMainModelChanges.bind(this));
-        },
-
-        onMainModelChanges: function() {
-        },
-
-        onNavigateChangeTariff: function () {
-            var router = sap.ui.core.UIComponent.getRouterFor(this);
-            Utils.navigateToMenuPageTab(router, "MyPolicies"); // TODO sample code, make real one
-        }
+        formatter: formatter
     });
 });
