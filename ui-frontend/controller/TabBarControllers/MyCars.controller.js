8804876deb30c9dea8ae65e23d29139314d8d9dd
dmitry <dmitry.zyryanov@execution.su>
2018-05-16 10:05:22 +0700

    my cars sample page: sample page added

---
+++ ui-frontend/controller/TabBarControllers/MyCars.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "personal/account/formatter/formatter"
+], function (Controller, formatter) {
+    "use strict";
+
+    return Controller.extend("personal.account.controller.TabBarControllers.MyCars", {
+        formatter: formatter
+    });
+});
